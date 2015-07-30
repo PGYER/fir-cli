d0e54a88999323e049e831a06012df6d3e05b81f
NaixSpirit <neverlandxy.naix@gmail.com>
2015-07-30 10:57:41 +0800

    add activesupport concern

--- lib/fir/patches.rb
+++ lib/fir/patches.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+require_relative './patches/concern'
 require_relative './patches/native_patch'
 require_relative './patches/os_patch'
 require_relative './patches/parser_patch'
