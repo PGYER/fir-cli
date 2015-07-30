80c23fa9e6c2ca00661bb84d4d293cabc521d079
NaixSpirit <neverlandxy.naix@gmail.com>
2015-07-30 16:53:51 +0800

    remove similar code

--- lib/fir/util/http.rb
+++ lib/fir/util/http.rb
@@ -3,71 +3,41 @@
 module FIR
   module Http
 
-    def get url, params = {}, timeout = 300
+    DEFAULT_TIMEOUT = 300
+
+    def get url, params = {}
       begin
         res = ::RestClient::Request.execute(
           method:  :get,
           url:     url,
-          timeout: timeout,
+          timeout: DEFAULT_TIMEOUT,
           headers: default_headers.merge(params: params)
         )
       rescue => e
-        logger.error "#{e.class}\n#{e.message}"
+        logger.error e.class.to_s + "\n" + e.message.to_s
         exit 1
       end
 
       JSON.parse(res.body.force_encoding("UTF-8"), symbolize_names: true)
     end
 
-    def post url, query, timeout = 300
-      begin
-        res = ::RestClient::Request.execute(
-          method:  :post,
-          url:     url,
-          payload: query,
-          timeout: timeout,
-          headers: default_headers
-        )
-      rescue => e
-        logger.error "#{e.class}\n#{e.message}"
-        exit 1
+    %w(post patch put).each do |method|
+      define_method method do |url, query|
+        begin
+          res = ::RestClient::Request.execute(
+            method:  method.to_sym,
+            url:     url,
+            payload: query,
+            timeout: DEFAULT_TIMEOUT,
+            headers: default_headers
+          )
+        rescue => e
+          logger.error e.class.to_s + "\n" + e.message.to_s
+          exit 1
+        end
+
+        JSON.parse(res.body.force_encoding("UTF-8"), symbolize_names: true)
       end
-
-      JSON.parse(res.body.force_encoding("UTF-8"), symbolize_names: true)
-    end
-
-    def patch url, query, timeout = 300
-      begin
-        res = ::RestClient::Request.execute(
-          method:  :patch,
-          url:     url,
-          payload: query,
-          timeout: timeout,
-          headers: default_headers
-        )
-      rescue => e
-        logger.error "#{e.class}\n#{e.message}"
-        exit 1
-      end
-
-      JSON.parse(res.body.force_encoding("UTF-8"), symbolize_names: true)
-    end
-
-    def put url, query, timeout = 300
-      begin
-        res = ::RestClient::Request.execute(
-          method:  :put,
-          url:     url,
-          payload: query,
-          timeout: timeout,
-          headers: default_headers
-        )
-      rescue => e
-        logger.error "#{e.class}\n#{e.message}"
-        exit 1
-      end
-
-      JSON.parse(res.body.force_encoding("UTF-8"), symbolize_names: true)
     end
 
     private
