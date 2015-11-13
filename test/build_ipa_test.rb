f4990fa20a65d830f3c2ce6fc79ff945166ba736
NaixSpirit <neverlandxy.naix@gmail.com>
2015-11-13 16:23:47 +0800

    resolve #36
    支持远程 git 项目打包编译

--- test/build_ipa_test.rb
+++ test/build_ipa_test.rb
@@ -9,6 +9,9 @@ class BuildAppTest < Minitest::Test
 
       assert FIR.build_ipa(default_ipa_project, options)
       assert FIR.build_apk(default_apk_project, options)
+
+      assert FIR.build_ipa(default_ipa_git_url, options)
+      assert FIR.build_apk(default_apk_git_url, options)
     end
   end
 end
