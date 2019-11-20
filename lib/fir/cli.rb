636b66c63e859bc207a4ed3ef74e8f75cfe17d97
atpking <atpking@gmail.com>
2019-11-20 12:47:41 +0800

    add aliases

--- lib/fir/cli.rb
+++ lib/fir/cli.rb
@@ -109,8 +109,8 @@ module FIR
     method_option :qrcode,      type: :boolean, aliases: '-Q', desc: 'Generate qrcode'
     method_option :need_release_id, type: :boolean, aliases: '-R', desc: 'Add release id with fir url (WARNING: FIR ONLY SAVED 30 releases recently per app'
 
-    method_option :force_pin_history, type: :boolean, default: false,  desc: 'pin this release to the download page by force'
-    method_option :skip_update_icon, type: :boolean, default: false,  desc: 'skip update app icon'
+    method_option :force_pin_history, type: :boolean, aliases: '-H', default: false,  desc: 'pin this release to the download page by force'
+    method_option :skip_update_icon, type: :boolean, aliases: '-S', default: false,  desc: 'skip update app icon'
     method_option :specify_icon_file, type: :string, desc: 'specify icon file'
 
     method_option :mappingfile, type: :string,  aliases: '-m', desc: 'App mapping file'
--- lib/fir/version.rb
+++ lib/fir/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module FIR
-  VERSION = '1.7.4'
+  VERSION = '1.7.4.1'
 end
